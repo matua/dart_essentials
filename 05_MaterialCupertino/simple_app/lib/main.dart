fc0b0451b0600dbd780ca5a4dc1095bf80280102
George <matuageorge@gmail.com>
2021-06-29 22:31:51 +0200

    Task 1

--- 05_MaterialCupertino/simple_app/lib/main.dart
+++ 05_MaterialCupertino/simple_app/lib/main.dart
@@ -5,23 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
@@ -31,20 +30,110 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'simple app',
-            ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        drawer: Drawer(
+          child: ListView(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          onTap: (int index) {},
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Photo"),
+            BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+            BottomNavigationBarItem(icon: Icon(Icons.album), label: "Albums"),
           ],
         ),
-      ),
+        floatingActionButton: MyFloatingActionButton(),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class MyFloatingActionButton extends StatefulWidget {
+  @override
+  _MyFloatingActionButtonState createState() => _MyFloatingActionButtonState();
+}
+
+class _MyFloatingActionButtonState extends State<MyFloatingActionButton> {
+  bool _show = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return _show
+        ? FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () {
+              var sheetController = showBottomSheet(
+                context: context,
+                builder: (context) => Container(
+                  height: 150,
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Icon(Icons.credit_card),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text('Сумма'),
+                                ),
+                              ),
+                              Expanded(
+                                child: Container(
+                                  alignment: Alignment.centerRight,
+                                  child: Text('200 руб'),
+                                ),
+                                flex: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                        ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.grey.shade400),
+                              textStyle: MaterialStateProperty.all(
+                                TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            child: const Text('Оплатить'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            })
+                      ],
+                    ),
+                  ),
+                ),
+              );
+              _showButton(false);
+              sheetController.closed.then((value) {
+                _showButton(true);
+              });
+            },
+          )
+        : Container();
+  }
+
+  void _showButton(bool value) {
+    setState(() {
+      _show = value;
+    });
   }
 }
