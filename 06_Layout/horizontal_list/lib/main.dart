9e2aca1090f241cb1963d7e16a6ff582e8149ebf
George <matuageorge@gmail.com>
2021-07-06 12:00:21 +0200

    Задание 3 Создайте прокручиваемый список. Если размер экрана меньше 500px, то список должен прокручиваться горизонтально, если больше — то список должен иметь вертикальный скролл. Критерии: Если размер экрана меньше 500px, то список прокручивается горизонтально, если больше — список имеет вертикальный скролл.

--- 06_Layout/horizontal_list/lib/main.dart
+++ 06_Layout/horizontal_list/lib/main.dart
@@ -13,13 +13,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  const MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -30,12 +30,51 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Horizontal list'),
+        title: const Text('Horizontal list'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraint) {
+                final double _screenWidth = constraint.constrainWidth();
+                const int _listLength = 10;
+                final List<int> colorCodes =
+                    List.generate(10, (int index) => 100 * index);
+                return _screenWidth < 500
+                    ? SizedBox(
+                        height: 100,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            padding: const EdgeInsets.all(8),
+                            itemCount: _listLength,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                height: 100,
+                                width: 100,
+                                color: Colors.green[colorCodes[index]],
+                                // child: Center(child: Text(entries[index])),
+                              );
+                            }),
+                      )
+                    : SizedBox(
+                        height: MediaQuery.of(context).size.height - 100,
+                        child: ListView.builder(
+                            padding: const EdgeInsets.all(8),
+                            itemCount: _listLength,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                height: 100,
+                                width: 100,
+                                color: Colors.green[colorCodes[index]],
+                                // child: Center(child: Text(entries[index])),
+                              );
+                            }),
+                      );
+              },
+            ),
+          ],
         ),
       ),
     );
