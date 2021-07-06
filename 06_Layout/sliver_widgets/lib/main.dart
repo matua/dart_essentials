ae4872518faa8ef04d4e41f2724d31a8f55518ee
George <matuageorge@gmail.com>
2021-07-06 07:51:44 +0200

    Задание 2 1. В SliverAppBar добавьте изображение, которое будет занимать весь размер SliverAppBar. 2. Добавьте полупрозрачный чёрный фон поверх этого изображения. 3. В SliverAppBar добавьте текст поверх изображения и полупрозрачного чёрного фона. При скролле изображение должно заменяться на стандартный синий фон SliverAppBar. При скролле страницы SliverAppBar должен оставаться вверху экрана.

--- 06_Layout/sliver_widgets/lib/main.dart
+++ 06_Layout/sliver_widgets/lib/main.dart
@@ -9,17 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  const MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -29,15 +30,73 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Sliver widgets',
+      drawer: const Drawer(),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            // backgroundColor: Colors.black.withOpacity(0.5),
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text('Page title'),
+              background: Image.network('https://picsum.photos/200/300',
+                  fit: BoxFit.cover),
             ),
-          ],
-        ),
+          ),
+          const SliverToBoxAdapter(
+            child: Text('Sed ut perspiciatis unde omnis iste natus error '
+                'sit voluptatem accusantium doloremque laudantium, totam rem'
+                ' aperiam, '
+                'eaque '
+                'ipsa quae ab illo inventore veritatis et quasi architecto'
+                ' beatae vitae '
+                'dicta sunt explicabo. Nemo enim ipsam voluptatem quia volu '
+                'ptas'
+                'sit '
+                'aspernatur aut odit aut fugit, sed quia consequuntur magni do '
+                'lores eos'
+                ' qui ratione voluptatem sequi nesciunt. Neque porro quisquam'
+                ' est, qui'
+                ' dolorem ipsum quia dolor sit amet, consectetur, adipisci vel '
+                'it, sed '
+                'quia non numquam eius modi tempora incidunt ut labore et dolo '
+                're magnam '
+                'aliquam quaerat voluptatem. Ut enim ad minima veniam, quis no '
+                'strum '
+                'exercitationem ullam corporis suscipit laboriosam, nisi ut al '
+                'iquid ex ea'
+                ' commodi consequatur? Quis autem vel eum iure reprehenderit q '
+                'ui in ea'
+                ' voluptate velit esse quam nihil molestiae consequatur, vel '
+                'illum qui '
+                'dolorem eum fugiat quo voluptas nulla pariatur? '
+                'Sed ut perspiciatis unde omnis iste natus error '
+                'sit voluptatem accusantium doloremque laudantium, totam rem'
+                ' aperiam, '
+                'eaque '
+                'ipsa quae ab illo inventore veritatis et quasi architecto be '
+                'atae vitae '
+                'dicta sunt explicabo. Nemo enim ipsam voluptatem quia volupt '
+                'as sit '
+                'aspernatur aut odit aut fugit, sed quia consequuntur magni d '
+                'olores eos'
+                ' qui ratione voluptatem sequi nesciunt. Neque porro quisquam '
+                ' est, qui'
+                ' dolorem ipsum quia dolor sit amet, consectetur, adipisci ve '
+                'lit, sed '
+                'quia non numquam eius modi tempora incidunt ut labore et dol '
+                'ore magnam '
+                'aliquam quaerat voluptatem. Ut enim ad minima veniam, quis n '
+                'ostrum '
+                'exercitationem ullam corporis suscipit laboriosam, nisi ut a '
+                'liquid ex ea'
+                ' commodi consequatur? Quis autem vel eum iure reprehenderit '
+                'qui in ea'
+                ' voluptate velit esse quam nihil molestiae consequatur, vel'
+                ' illum qui '
+                'dolorem eum fugiat quo voluptas nulla pariatur?'),
+          ),
+        ],
       ),
     );
   }
